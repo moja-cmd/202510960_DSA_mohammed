08ac138b58d66fde873c7e36e18ec2f6dad2de0d
moja-cmd <mohamedjamp@gmail.com>
2026-05-26 07:15:55 +0200

    Add files via upload

---
+++ assignment3mainprogram.cpp
@@ -0,0 +1,57 @@
+#include "assignment3Question2a.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int choice;
+
+    do
+    {
+        cout << "\n===== STUDENT MANAGEMENT SYSTEM =====\n";
+        cout << "1. Add Student\n";
+        cout << "2. Display All Students\n";
+        cout << "3. Search Student\n";
+        cout << "4. Update Student\n";
+        cout << "5. Delete Student\n";
+        cout << "6. Exit\n";
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+
+        case 1:
+            addStudent();
+            break;
+
+        case 2:
+            displayStudents();
+            break;
+
+        case 3:
+            searchStudent();
+            break;
+
+        case 4:
+            updateStudent();
+            break;
+
+        case 5:
+            deleteStudent();
+            break;
+
+        case 6:
+            cout << "Exiting program...\n";
+            break;
+
+        default:
+            cout << "Invalid choice.\n";
+        }
+
+    } while (choice != 6);
+
+    return 0;
+}
