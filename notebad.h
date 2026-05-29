b09b32860e063821c8a76558a70284efbd5ce10d
moja-cmd <mohamedjamp@gmail.com>
2026-05-29 07:21:43 +0200

    Add files via upload

---
+++ notebad.h
@@ -0,0 +1,11 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <string>
+
+// Function declarations for file operations
+void writeFile(std::string filename);
+void readFile(std::string filename);
+void appendFile(std::string filename);
+
+#endif
